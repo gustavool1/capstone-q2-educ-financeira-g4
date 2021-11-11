67b590a0db1205178de3d7256501de4313632476
Eric Esteves Martins <estevesmartinseric@gmail.com>
2021-11-10 22:14:00 -0300

    feat:globalstyle iniciado

---
+++ src/GlobalStyle/index.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+:root {
+    --color-primary: #9B5DE5;
+    --color-secondary: #f15bb5;
+    --color-tertiary:#fee440;
+    --color-quaternary:#00bbf9;
+    --color-quiternary:#00f5d4;
+    --color-hexaternary:#302F4D;
+    --gray-600: #333333;
+    --gray-300: #828282;
+    --gray-100: #e0e0e0;
+    --gray-0: #f5f5f5;
+    --negative: #e60000;
+    --warning: #ffcd07;
+    --sucess: #168821;
+    --information: #155bcb;
+    --white: F5F5F5;
+  }
+  
+  h1 {
+    font-weight: bold;
+    font-size: 26px;
+    margin: 0;
+  }
+  
+  h2 {
+    font-weight: bold;
+    font-size: 22px;
+    margin: 0;
+  }
+  
+  h3 {
+    font-weight: bold;
+    font-size: 18px;
+    margin: 0;
+  }
+  
+  p {
+    margin: 0;
+  }
+  
+  button {
+    cursor: pointer;
+  }
+`;
