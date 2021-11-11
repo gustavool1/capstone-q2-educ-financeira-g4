bdccc0492cb350603fb84c502cbb2e2810b3be23
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-11-11 10:43:04 -0300

    feat: adding the video div and saiba mais

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,3 +1,18 @@
+import { VideoContainer, HowItWorks } from "./style";
 export const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      <VideoContainer />
+      <HowItWorks>
+        <h2>Como Funciona ?</h2>
+        <p>
+          Você pode criar a uma conta para seu filho e adicionar atividades para
+          serem feitas com um devido valor a ser recebido pela atividade
+          realizada, além disso, possuímos conteúdos interativos para as
+          crianças aprenderem mais sobre economia e conseguir acompanhar o
+          próprio balanço financeiro.
+        </p>
+      </HowItWorks>
+    </>
+  );
 };
---
+++ src/Pages/Home/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const VideoContainer = styled.div`
+  height: 500px;
+`;
+
+export const HowItWorks = styled.div`
+  padding: 30px;
+  background-color: #9b5de5;
+  h2 {
+    color: #fff;
+  }
+  p {
+    color: #fff;
+  }
+`;
