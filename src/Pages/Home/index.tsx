1805fae294b3ffecb7bbd854ee8e8588d873bd60
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-11-12 10:20:46 -0300

    feat: adding the animated section

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -6,80 +6,125 @@ import {
   Steps,
   JoinToday,
   WhoWeAre,
+  AnimatedSection,
 } from "./style";
 import Dollar from "../../assets/images/Dollar.svg";
+import LottieMaker from "../../Components/LottieMaker";
+
 export const Home = () => {
   return (
     <>
       <VideoContainer />
+
       <HowItWorks>
-        <h2>Como Funciona ?</h2>
-        <p>
-          Você pode criar a uma conta para seu filho e adicionar atividades para
-          serem feitas com um devido valor a ser recebido pela atividade
-          realizada, além disso, possuímos conteúdos interativos para as
-          crianças aprenderem mais sobre economia e conseguir acompanhar o
-          próprio balanço financeiro.
-        </p>
+        <div className="Animation">
+          <LottieMaker
+            lottieImage="https://assets9.lottiefiles.com/packages/lf20_89z7ku.json"
+            height={500}
+            width={500}
+            playerAreVisible={false}
+          />
+        </div>
+        <div className="Content">
+          <h2>Como Funciona ?</h2>
+          <p>
+            Você pode criar a uma conta para seu filho e adicionar atividades
+            para serem feitas com um devido valor a ser recebido pela atividade
+            realizada, além disso, possuímos conteúdos interativos para as
+            crianças aprenderem mais sobre economia e conseguir acompanhar o
+            próprio balanço financeiro.
+          </p>
+        </div>
       </HowItWorks>
+
       <KnowMore>
-        <p>Saiba agora porque vocês precisam da gente!</p>
-        <button>Saiba Mais</button>
+        <div className="Content">
+          <p>Saiba agora porque vocês precisam da gente!</p>
+          <button>Saiba Mais</button>
+        </div>
+        <div className="Animation">
+          <LottieMaker
+            lottieImage="https://assets9.lottiefiles.com/packages/lf20_jjokptgh.json"
+            width={500}
+            height={500}
+            playerAreVisible={false}
+          />
+        </div>
       </KnowMore>
+
       <Content>
         <h2>Conteúdos</h2>
-        <p>
-          Você cria atividades para seus filhos e valores a serem dados pela
-          realização das tarefas.
-        </p>
+        <div>
+          <h3>Atividades</h3>
+          <p>
+            Você cria atividades para seus filhos e valores a serem dados pela
+            realização das tarefas.
+          </p>
 
-        <h3>Lista de desejos</h3>
-        <p>
-          Seu filho poderia registrar uma lista de coisas que deseja comprar e
-          colocaria o valor estipulado do item.
-        </p>
+          <h3>Lista de desejos</h3>
+          <p>
+            Seu filho poderia registrar uma lista de coisas que deseja comprar e
+            colocaria o valor estipulado do item.
+          </p>
+        </div>
+        <div>
+          <h3>Balanço economico</h3>
+          <p>
+            No site o seu filho registraria o dinheiro recebido e os próprios
+            gastos, podendo acompanhar seu dinheiro em caixa.
+          </p>
 
-        <h3>Balanço economico</h3>
-        <p>
-          No site o seu filho registraria o dinheiro recebido e os próprios
-          gastos, podendo acompanhar seu dinheiro em caixa.
-        </p>
-
-        <h3>Tutorial básico de finanças</h3>
-        <p>Uma página interatira ensinando o básico de ecônomia.</p>
+          <h3>Tutorial básico de finanças</h3>
+          <p>Uma página interativa ensinando o básico de economia.</p>
+        </div>
       </Content>
+
       <Steps>
         <h2>Passos</h2>
-        <div>
-          <h4>1. Crie a conta do seu filho</h4>
-          <p>
-            Crie a conta do dependente, posteriormente ele pode logar
-            diretamente com a própria conta e você poderá acompanhar os dados do
-            seu filho
-          </p>
-        </div>
+        <div className="Content">
+          <div>
+            <h4>1. Crie a conta do seu filho</h4>
+            <p>
+              Crie a conta do dependente, posteriormente ele pode logar
+              diretamente com a própria conta e você poderá acompanhar os dados
+              do seu filho
+            </p>
+          </div>
 
-        <div>
-          <h4>2. Crie atividades para ele(s)</h4>
-          <p>
-            Dentro de sua conta crie as atividade para seus filhos e coloque os
-            valores de recompensa
-          </p>
-        </div>
+          <div>
+            <h4>2. Crie atividades para ele(s)</h4>
+            <p>
+              Dentro de sua conta crie as atividade para seus filhos e coloque
+              os valores de recompensa
+            </p>
+          </div>
 
-        <div>
-          <h4>3. Pague seu filho e acompanhe os gastos deles</h4>
-          <p>
-            Quando a tarefa por concluida ele vai receber o valor e vai colocar
-            o quanto recebeu e o quanto gastou na parte de balanço financeiro.
-          </p>
+          <div>
+            <h4>3. Pague seu filho e acompanhe os gastos deles</h4>
+            <p>
+              Quando a tarefa por concluida ele vai receber o valor e vai
+              colocar o quanto recebeu e o quanto gastou na parte de balanço
+              financeiro.
+            </p>
+          </div>
         </div>
       </Steps>
+
+      <AnimatedSection>
+        <LottieMaker
+          lottieImage="https://assets5.lottiefiles.com/private_files/lf30_tcuqw7ib.json"
+          width={500}
+          height={500}
+          playerAreVisible={false}
+        />
+        <h2>Um aplicativo bem interativo e animado</h2>
+      </AnimatedSection>
 
       <JoinToday>
         <img src={Dollar} alt="Dollar representation" />
         <button>Cadastre-se</button>
       </JoinToday>
+
       <WhoWeAre>
         <h2>Quem somos</h2>
         <p>
--- src/Pages/Home/style.ts
+++ src/Pages/Home/style.ts
@@ -5,63 +5,96 @@ export const VideoContainer = styled.div`
 `;
 
 export const HowItWorks = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   padding: 30px;
   background-color: #9b5de5;
-  h2 {
-    color: #fff;
-    text-align: center;
-    margin-bottom: 10px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  .Animation {
+    display: none;
   }
-  p {
-    color: #fff;
-    font-size: 18px;
-    line-height: 30px;
-    letter-spacing: -0.015em;
+  .Content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h2 {
+      color: #fff;
+      text-align: center;
+      margin-bottom: 25px;
+      font-size: 24px;
+    }
+    p {
+      color: #fff;
+      font-size: 18px;
+      line-height: 30px;
+      letter-spacing: -0.015em;
+      max-width: 600px;
+    }
+  }
+  @media (min-width: 768px) {
+    .Animation {
+      display: initial;
+    }
+    .Content {
+      h2 {
+        width: 100%;
+        text-align: left;
+      }
+    }
   }
 `;
 
 export const KnowMore = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
   background-color: #302f4d;
   padding: 65px 30px;
-  display: flex;
-  flex-direction: column;
-
-  p {
-    font-size: 28px;
-    text-align: center;
-    color: #fff;
-    width: 220px;
-    margin: 0 auto 45px auto;
+  justify-content: space-around;
+  align-items: center;
+  .Animation {
+    display: none;
   }
-  button {
-    padding: 20px;
-    background-color: transparent;
-    border: 2px solid #fff;
-    color: #fff;
-    margin: 0 auto;
-    border-radius: 50px;
-    font-size: 28px;
-    width: 240px;
+  .Content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    p {
+      font-size: 28px;
+      text-align: center;
+      color: #fff;
+      margin: 0 auto 45px auto;
+      max-width: 313px;
+    }
+    button {
+      padding: 20px;
+      background-color: transparent;
+      border: 2px solid #fff;
+      color: #fff;
+      margin: 0 auto;
+      border-radius: 50px;
+      font-size: 28px;
+      width: 240px;
+    }
+  }
+  @media (min-width: 768px) {
+    .Animation {
+      display: initial;
+    }
   }
 `;
 
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   padding: 30px;
   background-color: #00bbf9;
+  align-items: center;
+
   h2 {
     margin: 0 0 65px 0;
     color: #fff;
     font-size: 48px;
     font-weight: normal;
+    width: 100%;
   }
   p {
     font-size: 18px;
@@ -75,12 +108,19 @@ export const Content = styled.div`
     margin-bottom: 5px;
     width: 100%;
   }
+  @media (min-width: 768px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding: 80px;
+    padding-bottom: 200px;
+    justify-content: space-between;
+    div {
+      width: 40%;
+    }
+  }
 `;
 
 export const Steps = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   padding: 20px;
   background-color: #8c218e;
   h2 {
@@ -90,26 +130,58 @@ export const Steps = styled.div`
     font-weight: normal;
     text-align: center;
   }
-  div {
-    border: 3px solid #ffffff;
-    border-radius: 10px;
-    height: 237px;
-    margin: 30px auto;
-    padding: 20px;
+  .Content {
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
-    width: 100%;
-    h4 {
-      color: #fff;
-      margin-bottom: 20px;
-      text-align: center;
-      font-size: 20px;
+    align-items: center;
+
+    div {
+      border: 3px solid #ffffff;
+      border-radius: 10px;
+      height: 237px;
+      margin: 30px auto;
+      padding: 20px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      width: 100%;
+      h4 {
+        color: #fff;
+        margin-bottom: 20px;
+        text-align: center;
+        font-size: 20px;
+      }
+      p {
+        color: #fff;
+        text-align: center;
+      }
     }
-    p {
-      color: #fff;
-      text-align: center;
+  }
+  @media (min-width: 768px) {
+    .Content {
+      flex-direction: row;
+      div {
+        width: 360px;
+        height: 400px;
+        :hover {
+          scale: 1.3;
+        }
+      }
     }
+  }
+`;
+export const AnimatedSection = styled.div`
+  display: none;
+  background: #fee440;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 615px;
+  h2 {
+    font-size: 48px;
+    width: 600px;
+  }
+  @media (max-width: 768) {
   }
 `;
 export const JoinToday = styled.div`
@@ -128,10 +200,13 @@ export const JoinToday = styled.div`
     background-color: transparent;
     border: 2px solid #fff;
     color: #fff;
-    margin: 0 auto;
     border-radius: 50px;
     font-size: 28px;
     width: 240px;
+  }
+  @media (min-width: 768px) {
+    flex-direction: row-reverse;
+    justify-content: space-around;
   }
 `;
 
@@ -151,5 +226,6 @@ export const WhoWeAre = styled.div`
     font-size: 36px;
     text-align: center;
     margin: 50px 0;
+    max-width: 1300px;
   }
 `;
