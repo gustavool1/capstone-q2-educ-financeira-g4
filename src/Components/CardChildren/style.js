4d04092c8662ec91c3e961485305843f97f871e6
Gustavo <go8895806@gmail.com>
2021-11-15 14:48:04 -0300

    fix:fixed the style of the cards

--- src/Components/CardChildren/style.js
+++ src/Components/CardChildren/style.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+   
+   
+    
+`
+
+export const Front = styled.div`
     width: 350px;
     height: 570px;
     margin: 10px;
@@ -10,18 +16,19 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
-    img{
-        border-radius:50%;
-    }
-    p{
-        max-width: 15ch;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        
-    }
-   
+    align-items: center;
     color: white;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    border: 1px solid white;
+    img{
+        width: 250px;
+        height: 250px;
+        border-radius: 50%;
+    }
+    
     .create-activity {
       padding: 10px;
       background-color: transparent;
@@ -32,37 +39,37 @@ export const Container = styled.div`
       font-size: 28px;
       width: 240px;
       :hover {
-      transition: 1.3s;
-      color: #000;
-      scale: 1.5;
-      background-color: #fff;
+        transition: 1.3s;
+        color: #000;
+        scale: 1.5;
+        background-color: #fff;
         }
     }
-`
-
-export const InfoContainer = styled.div`
-    display: flex;
-    padding: 10px;
-    height: 25%;
-    div{
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end;
-        justify-content: space-around;
-        p{
-            font-weight: bold;
-        }
+    p{
+        font-size: 25px;
     }
 `
-export const ActivitiesContainer = styled.div`
-    padding: 10px;
+export const Back = styled.div`
+    width: 350px;
+    height: 570px;
+    margin: 10px;
+    padding: 2%;
+    border-radius: 15px;
+    background-color: #302F4D;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: space-evenly;
+    align-items: center;
+    color: white;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    border: 1px solid white;
 `
 export const Achivied = styled.div`
     height: 170px;
+    width: 90%;
     overflow: auto;
     overflow-x:hidden;
     div{
@@ -70,6 +77,7 @@ export const Achivied = styled.div`
         justify-content: space-between;
         align-items: center;
         margin:5px;
+
        
         p:nth-child(1){
             width: 50%;
@@ -85,6 +93,7 @@ export const Achivied = styled.div`
 export const NotAchivied = styled.div`
    height: 170px;
    overflow: auto;
+   width: 90%;
    div{
         display: flex;
         justify-content: space-between;
@@ -117,5 +126,28 @@ export const NotAchivied = styled.div`
         }
         
       
+    }
+`
+
+export const ButtonsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    .create-activity {
+      padding: 10px;
+      background-color: transparent;
+      border: 2px solid #fff;
+      color: #fff;
+      margin: 0 auto;
+      border-radius: 5px;
+      font-size: 20px;
+      margin: 5px;
+      width: 50%;
+      :hover {
+        transition: 1.3s;
+        color: #000;
+        scale: 1.5;
+        background-color: #fff;
+        }
     }
 `
